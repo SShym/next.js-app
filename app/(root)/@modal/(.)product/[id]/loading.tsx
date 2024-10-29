aa01fe15cadd1e2882f42ca1086d8a04c8aa1827
Serhiy <serh.shym@gmail.com>
2024-10-29 14:07:47 +0300

    fix spinner position

--- app/(root)/@modal/(.)product/[id]/loading.tsx
+++ app/(root)/@modal/(.)product/[id]/loading.tsx
@@ -2,7 +2,7 @@ import Spinner from "@/shared/components/ui/spinner";
 
 export default function Loading() {
   return (
-    <div className='absolute left-0 right-0 top-0 bottom-0 flex items-center justify-center w-full h-full'>
+    <div className='fixed inset-0 flex items-center justify-center'>
       <Spinner height={50} width={50} />
     </div>
   );
